ff47fd6a3237e3f167a472bb2808f1ceb199b6a5
Brandon <branyzp@gmail.com>
2022-11-16 20:35:58 +0800

    added change password

--- src/components/ChangePw.jsx
+++ src/components/ChangePw.jsx
@@ -1,20 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useContext } from 'react';
 import { DarkModeContext } from '../context/DarkModeContext';
+import axios from 'axios';
 
-function ChangePw() {
+function ChangePw({ userdetails }) {
 	const { darkmode } = useContext(DarkModeContext);
+	const [updatePw, setUpdatePw] = useState(false);
+	const [changePw, setChangePw] = useState(' ');
+	const [checkChangePw, setCheckChangePw] = useState(' ');
+	const [oldPw, setOldPw] = useState(' ');
+
+	let updatepw_api = 'http://localhost:8000/api/updatepw';
+	console.log(updatePw);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setUpdatePw(!updatePw);
+
+		axios
+			.put(updatepw_api, {
+				oldPassword: oldPw,
+				newPassword: changePw,
+				id: userdetails[0],
+			})
+			.then((res) => console.log(res.data));
+	};
+
+	const toggleUpdateMode = (e) => {
+		e.preventDefault();
+		setUpdatePw(!updatePw);
+	};
+
 	return (
 		<div className="py-2">
-			<button
-				className={
-					darkmode
-						? 'rounded-xl bg-yellow-500 hover:bg-yellow-400 text-slate-700 font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-600 hover:scale-105 ease-in duration-100'
-						: 'rounded-xl bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 hover:scale-105 ease-in duration-100'
-				}
-			>
-				Change Password
-			</button>
+			{updatePw ? (
+				<form onSubmit={(e) => handleSubmit(e)}>
+					<p className="py-2">
+						Current password:{' '}
+						<input
+							type="password"
+							className=" bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							onChange={(e) => {
+								setOldPw(e.target.value);
+							}}
+							required="password"
+						></input>
+					</p>
+					<p className="py-2">
+						New password:{' '}
+						<input
+							className=" bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							onChange={(e) => {
+								setChangePw(e.target.value);
+							}}
+							type="password"
+							required="password"
+						></input>
+					</p>
+					<p className="py-2">
+						Confirm new password:{' '}
+						<input
+							type="password"
+							className=" bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							onChange={(e) => {
+								setChangePw(e.target.value);
+							}}
+							required="password"
+						></input>
+					</p>
+					<button
+						type="submit"
+						className={
+							darkmode
+								? ' mx-2 rounded-xl bg-yellow-500 hover:bg-yellow-400 text-slate-700 font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-600 hover:scale-105 ease-in duration-100'
+								: 'mx-2 rounded-xl bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 hover:scale-105 ease-in duration-100'
+						}
+					>
+						Change Password
+					</button>
+					<button
+						onClick={(e) => toggleUpdateMode(e)}
+						className={
+							darkmode
+								? ' rounded-xl bg-yellow-500 hover:bg-yellow-400 text-slate-700 font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-600 hover:scale-105 ease-in duration-100'
+								: 'rounded-xl bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 hover:scale-105 ease-in duration-100'
+						}
+					>
+						Back
+					</button>
+				</form>
+			) : (
+				<button
+					onClick={(e) => toggleUpdateMode(e)}
+					className={
+						darkmode
+							? 'rounded-xl bg-yellow-500 hover:bg-yellow-400 text-slate-700 font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-600 hover:scale-105 ease-in duration-100'
+							: 'rounded-xl bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 hover:scale-105 ease-in duration-100'
+					}
+				>
+					Change Password
+				</button>
+			)}
 		</div>
 	);
 }
--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -13,7 +13,6 @@ function UserDetails({ userdetails }) {
 	const [updatepwMode, setUpdatepwMode] = useState(false);
 
 	let updateuserdetails_api = 'http://localhost:8000/api/updateuser';
-	let updatepw_api = 'http://localhost:8000/api/updatepw';
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -43,7 +42,7 @@ function UserDetails({ userdetails }) {
 					: 'min-h-screen py-10'
 			}
 		>
-			<div className="my-32 text-center">
+			<div className="flex my-32 text-center items-center justify-center">
 				{updateMode ? (
 					<div>
 						<h1 className="text-3xl font-bold tracking-wide">
@@ -54,7 +53,7 @@ function UserDetails({ userdetails }) {
 							<p className="py-2">
 								Email:{' '}
 								<input
-									className="text-black"
+									className=" bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									type="email"
 									required
 									onChange={(e) => {
@@ -68,7 +67,7 @@ function UserDetails({ userdetails }) {
 								<input
 									type="text"
 									required
-									className="text-black"
+									className=" bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									onChange={(e) => {
 										setNewFirstname(e.target.value);
 									}}
@@ -80,7 +79,7 @@ function UserDetails({ userdetails }) {
 								<input
 									type="text"
 									required
-									className="text-black"
+									className=" bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									onChange={(e) => {
 										setNewLastname(e.target.value);
 									}}
@@ -98,28 +97,32 @@ function UserDetails({ userdetails }) {
 								Update User Details
 							</button>
 						</form>
+						<ChangePw userdetails={userdetails} />
 					</div>
 				) : (
 					<div>
-						<h1 className="text-3xl font-bold tracking-wide">Dashboard Page</h1>
-						<p className=" text-md py-2">Welcome, {newFirstname}.</p>
-						<p className="text-md py-2">Email: {newEmail}</p>
-						<p className="text-md py-2">First Name: {newFirstname}</p>
-						<p className="text-md py-2">Last Name: {newLastname}</p>
-						<button
-							className={
-								darkmode
-									? 'rounded-xl bg-yellow-500 hover:bg-yellow-400 text-slate-700 font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-600 hover:scale-105 ease-in duration-100'
-									: 'rounded-xl bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 hover:scale-105 ease-in duration-100'
-							}
-							onClick={() => toggleUpdateMode()}
-						>
-							Update User Details
-						</button>
+						<h1 className="text-3xl font-bold tracking-wide">
+							User Details Page
+						</h1>
+						<div>
+							<p className=" text-md py-2">Welcome, {newFirstname}.</p>
+							<p className="text-md py-2">Email: {newEmail}</p>
+							<p className="text-md py-2">First Name: {newFirstname}</p>
+							<p className="text-md py-2">Last Name: {newLastname}</p>
+							<button
+								className={
+									darkmode
+										? 'rounded-xl bg-yellow-500 hover:bg-yellow-400 text-slate-700 font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-600 hover:scale-105 ease-in duration-100'
+										: 'rounded-xl bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 hover:scale-105 ease-in duration-100'
+								}
+								onClick={() => toggleUpdateMode()}
+							>
+								Update User Details
+							</button>
+							<ChangePw userdetails={userdetails} />
+						</div>
 					</div>
 				)}
-
-				<ChangePw />
 			</div>
 		</div>
 	);
